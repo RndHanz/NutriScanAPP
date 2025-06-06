ff00b145388f8053186009466edb0a193db1eaa3
Raihan Nur Daffa <nurdaffaraihan46@gmail.com>
2025-06-06 19:40:52 +0700

    add profile logic

--- app/src/main/java/com/example/nutriscanapp/ProfileActivity.java
+++ app/src/main/java/com/example/nutriscanapp/ProfileActivity.java
@@ -1,4 +1,31 @@
 package com.example.nutriscanapp;
 
-public class ProfileActivity {
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    EditText etUsia, etAlergi, etTinggi, etBerat;
+    LinearLayout btnLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity);
+
+        etUsia = findViewById(R.id.etUsia);
+        etAlergi = findViewById(R.id.etAlergi);
+        etTinggi = findViewById(R.id.etTinggi);
+        etBerat = findViewById(R.id.etBerat);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(v -> {
+            // TODO: Tambahkan logika logout
+            finish();
+        });
+    }
 }
