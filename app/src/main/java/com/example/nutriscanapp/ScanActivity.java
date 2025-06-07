62ac5ebc3c522212fa57db9b276c5921042ffc30
Raihan Nur Daffa <nurdaffaraihan46@gmail.com>
2025-06-07 20:13:03 +0700

    add fitur scanner

--- app/src/main/java/com/example/nutriscanapp/ScanActivity.java
+++ app/src/main/java/com/example/nutriscanapp/ScanActivity.java
@@ -1,4 +1,43 @@
 package com.example.nutriscanapp;
 
-public class ScanActivity {
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+public class ScanActivity extends AppCompatActivity {
+    private DecoratedBarcodeView barcodeScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        barcodeScannerView = findViewById(R.id.barcodeScannerView);
+        barcodeScannerView.decodeContinuous(callback);
+    }
+
+    private final BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            barcodeScannerView.pause(); // Hentikan setelah scan
+            Toast.makeText(ScanActivity.this, "Hasil: " + result.getText(), Toast.LENGTH_SHORT).show();
+
+            // TODO: Lanjutkan ke activity detail / kirim ke database
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        barcodeScannerView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        barcodeScannerView.pause();
+    }
 }
